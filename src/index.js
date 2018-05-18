e07eef620bdaf2035859b9d9a29b494fe5eedbf1
Louise van den Berg <louisevdb84@gmail.com>
2018-05-18 16:52:01 +0200

    Add routing and a sidebar

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+
 import './index.css';
-import Time from './App/Time/Views/Time';
+import indexRoutes from "./routes/index";
 import registerServiceWorker from './registerServiceWorker';
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/scss/now-ui-dashboard.css?v=1.1.0";
 import "./assets/css/demo.css";
 
+const hist = createBrowserHistory();
 const logger = createLogger();
-
 const rootReducer = combineReducers({});
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
-    <Provider  store={store}>
-        <Time />
-    </Provider>, document.getElementById('root'));    
-    registerServiceWorker();
+    <Router history={hist}>    
+        <Provider store={store}>
+        <Switch>
+            {indexRoutes.map((prop, key) => {
+            return <Route path={prop.path} key={key} component={prop.component} />;
+            })}
+    </Switch>
+    </Provider>
+    </Router>, document.getElementById('root'));    
+        registerServiceWorker();
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import Time from "../App/Time/Views/Time"
+
+var indexRoutes = [
+    {path: "/", name: "Time", component: Time}
+];
+
+export default indexRoutes;
